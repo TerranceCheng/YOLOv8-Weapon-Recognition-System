f40875eeba64c79181f6b4384bca66f78b189deb
TerranceCheng <yongtat.cheng@gmail.com>
2024-03-14 21:42:03 +0800

    Updates

--- dev/client_side/yoloTrain.py
+++ dev/client_side/yoloTrain.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
 
 # Create model
-model = YOLO('yolov8n.yaml')
+model = YOLO('yolov8n.pt')
 
 # Train model
 results = model.train(data='dev\client_side\config.yaml', epochs = 1)
+model.export(format="onnx",opset=12)
