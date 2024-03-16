0bbf4242ca4ac5010c28c717b59848d7c347d6ba
TerranceCheng <yongtat.cheng@gmail.com>
2024-03-16 19:50:00 +0800

    Update yoloTrain.py

--- dev/client_side/yoloTrain.py
+++ dev/client_side/yoloTrain.py
@@ -1,8 +1,34 @@
+# !!!!!!!!!!! CPU !!!!!!!!!!!
+
 from ultralytics import YOLO
 
 # Create model
-model = YOLO('yolov8n.pt')
+model = YOLO('best.pt')
 
 # Train model
-results = model.train(data='dev\client_side\config.yaml', epochs = 1)
+results = model.train(data='dev\client_side\config.yaml', epochs=50)
 model.export(format="onnx",opset=12)
+
+# !!!!!!!!!!! TORCH CUDA, GPU !!!!!!!!!!!
+
+# from ultralytics import YOLO
+# import torch
+
+# device = '0' if torch.cuda.is_available() else "cpu"
+# if device == '0':
+#     torch.cuda.set_device(0)
+
+
+# # Load a model
+# # model = YOLO('best.pt')  # build a new model from scratch
+# # results=model('a.jpeg')
+# # model.to(device)
+# # model.train(data="dev\client_side\config.yaml", epochs=5)
+# print ('Device:', device)
+
+# model = YOLO('best.pt')
+
+# model.to(device='cuda')
+# print ('Before:', model.device.type)
+# results=model('a.jpeg')
+# print("After:", model.device.type)
