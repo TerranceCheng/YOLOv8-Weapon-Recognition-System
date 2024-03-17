6df4d07c228494bb8f97b432b0c07eec6bb24e06
TerranceCheng <yongtat.cheng@gmail.com>
2024-03-17 14:40:28 +0800

    Update detection.py

--- dev/client_side/detection.py
+++ dev/client_side/detection.py
@@ -1,90 +1,132 @@
-from PyQt5.QtCore import QThread, Qt, pyqtSignal
-from PyQt5.QtGui import QImage
-import cv2
-import time
-import numpy as np
+# from PyQt5.QtCore import QThread, Qt, pyqtSignal
+# from PyQt5.QtGui import QImage
+# from ultralytics import YOLO
+# import cv2
+# import cvzone
+# import math
+# import numpy as np
 
-class Detection(QThread):
+# class Detection(QThread):
+#     def __init__(self):
+#         super(Detection, self).__init__()
+
+#     changePixmap = pyqtSignal(QImage)
+
+#     def run(self):
+#         self.running = True
+
+#         # Reading the classes
+#         with open('obj.names', 'r') as f:
+#             classes = [line.strip() for line in f.readlines()] 
+
+#         cap = cv2.VideoCapture(0)
+#         model = YOLO('best.pt')
+
+#         while True:
+#             ret, frame = cap.read()
+#             frame = cv2.resize(frame, (640, 480))
+#             result = model(frame, stream=True)
+            
+#             # Getting bbox, confidence and class names informations to work with
+#             for info in result:
+#                 boxes = info.boxes
+
+#                 for box in boxes:
+#                     confidence = box.conf[0]
+#                     confidence = math.ceil(confidence * 100)
+#                     Class = int(box.cls[0])
+
+#                     if confidence > 50:
+#                         x1, y1, x2, y2 = box.xyxy[0]
+#                         x1, y1, x2, y2 = int(x1), int(y1), int (x2), int(y2)
+#                         cv2.rectangle(frame, (x1,y1), (x2,y2), (0,0,255), 5)
+#                         cvzone.putTextRect(frame, f'{classes[Class]} {confidence}%', [x1 + 8, y1 + 100], scale=1.5, thickness=2)
+
+#             cv2.imshow('frame', frame)
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from ultralytics import YOLO
+import cvzone
+import math
+import sys
+import cv2
+
+class Detection(QWidget):
     def __init__(self):
         super(Detection, self).__init__()
 
-    changePixmap = pyqtSignal(QImage)
+        # changePixmap = pyqtSignal(QImage) // REMEMBER TO CHECK HOW TO LINK THIS
 
+        self.VBL = QVBoxLayout()
+
+        self.FeedLabel = QLabel()
+        self.VBL.addWidget(self.FeedLabel)
+
+        self.CancelBTN = QPushButton("Cancel")
+        self.CancelBTN.clicked.connect(self.CancelFeed)
+        self.VBL.addWidget(self.CancelBTN)
+
+        self.Worker1 = Worker1()
+        
+        self.Worker1.start()
+        self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
+
+        self.setLayout(self.VBL)
+
+    def ImageUpdateSlot(self, Image):
+        self.FeedLabel.setPixmap(QPixmap.fromImage(Image))
+
+    def CancelFeed(self):
+        self.Worker1.stop()
+
+
+class Worker1(QThread):
+    ImageUpdate = pyqtSignal(QImage)
     def run(self):
-        self.running = True
-
-        net = cv2.dnn.readNet('yolov8s.onnx')
-        classes = []
+        self.ThreadActive = True
 
         with open('obj.names', 'r') as f:
-            classes = [line.strip() for line in f.readLines()] 
+            classes = [line.strip() for line in f.readlines()] 
 
-        layer_names = net.getLayerNames()
-        output_layers = [layer_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
-        colors = np.random.uniform(0, 255, size=(len(classes), 3))
-
-        font = cv2.FONT_HERSHEY_PLAIN
-        starting_time = time.time()
-
-
-        # 0 = webcam, 1 = external cam
+        model = YOLO('best.pt')
         cap = cv2.VideoCapture(0)
 
-        while self.running:
+        while self.ThreadActive:
             ret, frame = cap.read()
-
             if ret:
-                height, width, channels = frame.shape
+                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                FlippedImage = cv2.flip(Image, 1)
+                ConvertToQtFormat = QImage(FlippedImage.data, FlippedImage.shape[1], FlippedImage.shape[0], QImage.Format_RGB888)
+                Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.ImageUpdate.emit(Pic)
+        
+            ret, frame = cap.read()
+            frame = cv2.resize(frame, (640, 480))
+            result = model(frame, stream=True)
+            
+            # Getting bbox, confidence and class names informations to work with
+            for info in result:
+                boxes = info.boxes
 
-                blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-                net.setInput(blob)
-                outs = net.forward(output_layers)
+                for box in boxes:
+                    confidence = box.conf[0]
+                    confidence = math.ceil(confidence * 100)
+                    Class = int(box.cls[0])
 
-                class_ids = []
-                confidences = []
-                boxes = []
-                for out in outs:
-                    for detection in out:
-                        scores = detection[5:]
-                        class_id = np.argmax(scores)
-                        confidence = scores[class_id]
+                    if confidence > 50:
+                        x1, y1, x2, y2 = box.xyxy[0]
+                        x1, y1, x2, y2 = int(x1), int(y1), int (x2), int(y2)
+                        cv2.rectangle(frame, (x1,y1), (x2,y2), (0,0,255), 5)
+                        cvzone.putTextRect(frame, f'{classes[Class]} {confidence}%', [x1 + 8, y1 + 100], scale=1.5, thickness=2)
 
-                        if confidence > 0.98:
-                            center_x = int(detection[0]*width)
-                            center_y = int(detection[1]*height)
-                            w = int(detection[2]*width)
-                            h = int(detection[3]*height)
+    def stop(self):
+        self.ThreadActive = False
+        self.quit()
 
-                            x = int(center_x-w/2)
-                            y = int(center_y-h/2)
-
-                            boxes.append([x, y, w, h])
-                            confidences.append(float(confidence))
-                            class_ids.append(class_id)
-
-                indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.8, 0.3)
-
-                for i in range(len(boxes)):
-                    if i in indexes:
-                        x, y, w, h = boxes[i]
-                        label = str(classes[class_ids[i]])
-                        confidence = confidences[i]
-                        color = (256, 0, 0)
-                        cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
-                        cv2.putText(frame, label + '{0:.1%}'.format(confidence), (x, y-20), font, 3, color, 3)
-
-                        elapsed_time = starting_time - time.time()
-
-                        if elapsed_time <= -10:
-                            starting_time = time.time()
-                            self.save_detection(frame)
-
-                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                bytesPerLine = channels * width
-                convertToQtFormat = QImage(rgbImage.data, width, height, bytesPerLine, QImage.Format_RGB888)
-                p = convertToQtFormat.scaled(1101, 571, Qt.KeepAspectRatio)
-                self.changePixmap.emit(p)
-    
-    def save_detection(self, frame):
-        cv2.imwrite('saved_frame/frame.jpg', frame)
-        print('Frame Saved')
+if __name__== "__main__":
+    App = QApplication(sys.argv)
+    Root = Detection()
+    Root.show()
+    sys.exit(App.exec())
